0f9a3761b4aa833f327261b831619d99fcf0487f
Nadav Avidor <na364@cam.ac.uk>
2019-09-13 19:29:52 +0100

    moved config_job_params.m to UI/

--- UI/config_job_params.m
+++ UI/config_job_params.m
@@ -11,8 +11,8 @@ proj_name = 'noProj'; % Folder under which MD results will be saved
 % as 'slurm')
 distributed_computing = 0;
 dist_comp_cmd = 'sbatch ~/pigle_sweep_slurm_submit.peta4-skylake';
-n_cores_available = 2; %  How many parallel processes can be used on the target machine
+n_cores_available = 6; %  How many parallel processes can be used on the target machine
 
 % Define the parameter space and values to be probed.
-sweepParams = {'eta','T'};
-sweepVal = {{[1],[2]},{300}};
+sweepParams = {'eta','mu0'};
+sweepVal = {{10},{0}};
